502f5ca02a2992607d2339a87f7a00f3b9e559cd
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2021-08-18 17:41:20 +0800

    switch to Lean's lede

--- script/build-openwrt.sh
+++ script/build-openwrt.sh
@@ -1,153 +1,111 @@
 #!/bin/bash
 
-green="\033[32m"
-white="\033[0m"
-yellow="\033[33m"
-red="\033[31m"
-
-cd $GITHUB_WORKSPACE
-
-echo -e "$green>>>> workdir: $(pwd) $white \n"
+GREEN="\033[32m"
+WHITE="\033[0m"
+YELLOW="\033[33m"
+RED="\033[31m"
 
 # openwrt源码库
-REPO_URL=https://github.com/immortalwrt/immortalwrt
-REPO_BRANCH=openwrt-18.06-k5.4
-REPO_FLODER=openwrt
+#REPO_URL=
+#REPO_BRANCH=
+#REPO_FLODER=
 
 # 机型配置文件库
-CONFIG_REPO=https://github.com/EnnawYang/Actions-OpenWrt
-CONFIG_BRANCH=main
-CONFIG_FLODER=Actions-OpenWrt
+#CONFIG_REPO=
+#CONFIG_BRANCH=
+#CONFIG_FLODER=
 
-update_config_repo () {
-    if [ -d $GITHUB_WORKSPACE/$CONFIG_FLODER/.git ]; then
-        cd $GITHUB_WORKSPACE/$CONFIG_FLODER
+WORKDIR=$(pwd)
+echo -e "$GREEN>>>> workdir: $WORKDIR $WHITE \n"
+
+UPDATE_CONFIG_REPO () {
+    if [ -d $WORKDIR/$CONFIG_FLODER/.git ]; then
+        cd $WORKDIR/$CONFIG_FLODER
         git reset --hard
-        echo -e "$green>>>> update config ... $white"
+        echo -e "$GREEN>>>> update config ... $WHITE"
         git pull
     else
-        echo -e "$green>>>> clone config ... $white"
-        cd $GITHUB_WORKSPACE
+        echo -e "$GREEN>>>> clone config ... $WHITE"
+        cd $WORKDIR
         git clone $CONFIG_REPO -b $CONFIG_BRANCH $CONFIG_FLODER --single-branch
     fi
 }
 
-update_source_code () {
-    if [ -d $GITHUB_WORKSPACE/$REPO_FLODER/.git ]; then
-        cd $GITHUB_WORKSPACE/$REPO_FLODER
+UPDATE_SOURCE_CODE () {
+    if [ -d $WORKDIR/$REPO_FLODER/.git ]; then
+        cd $WORKDIR/$REPO_FLODER
         git reset --hard
-        distclean
-        echo -e "$green>>>> update source code ... $white"
+        echo -e "$GREEN>>>> update source code ... $WHITE"
         git pull
         ./scripts/feeds update -a && ./scripts/feeds install -a
     else
-        echo -e "$green>>>> clone source code ... $white"
-        cd $GITHUB_WORKSPACE
+        echo -e "$GREEN>>>> clone source code ... $WHITE"
+        cd $WORKDIR
         git clone $REPO_URL -b $REPO_BRANCH $REPO_FLODER --single-branch
-        cd $GITHUB_WORKSPACE/$REPO_FLODER
+        cd $WORKDIR/$REPO_FLODER
         ./scripts/feeds update -a && ./scripts/feeds install -a
     fi
 }
 
-distclean () {
-    if [[ $MAKE_DISTCLEAN == true ]]; then
-        cd $GITHUB_WORKSPACE/$REPO_FLODER
-        make distclean
+RM_DL () {
+    if [[ $RM_DL == true ]] && [ -d $WORKDIR/$REPO_FLODER/dl ]; then
+        rm -rf $WORKDIR/$REPO_FLODER/dl
     fi
 }
 
-rm_dl () {
-    if [[ $RM_DL == true ]] && [ -d $GITHUB_WORKSPACE/$REPO_FLODER/dl ]; then
-        cd $GITHUB_WORKSPACE/$REPO_FLODER
-        rm -rf dl
-    fi
+CUSTOM_CONFIGURATION () {
+    cd $WORKDIR/$REPO_FLODER
+    echo -e "$GREEN>>>> load custom configuration ... $WHITE"
+    chmod +x $WORKDIR/$CONFIG_FLODER/*.sh
+    bash $WORKDIR/$CONFIG_FLODER/"$TARGET_NAME"-part2.sh
 }
 
-custom_configuration () {
-    cd $GITHUB_WORKSPACE/$REPO_FLODER
-    echo -e "$green>>>> load custom configuration ... $white"
-    chmod +x $GITHUB_WORKSPACE/$CONFIG_FLODER/*.sh
-    bash $GITHUB_WORKSPACE/$CONFIG_FLODER/"$TARGET_NAME"-part2.sh
-}
-
-copy_config () {
-    echo -e "$green>>>> copy .config file ... $white"
-    if [ -e $GITHUB_WORKSPACE/$REPO_FLODER/.config ];then
-        rm $GITHUB_WORKSPACE/$REPO_FLODER/.config*
+COPY_CONFIG () {
+    echo -e "$GREEN>>>> copy .config file ... $WHITE"
+    if [ -e $WORKDIR/$REPO_FLODER/.config ];then
+        rm $WORKDIR/$REPO_FLODER/.config*
     fi
-    cp $GITHUB_WORKSPACE/$CONFIG_FLODER/"$TARGET_NAME".config $GITHUB_WORKSPACE/$REPO_FLODER/.config
-    cd $GITHUB_WORKSPACE/$REPO_FLODER
+    cp $WORKDIR/$CONFIG_FLODER/"$TARGET_NAME".config $WORKDIR/$REPO_FLODER/.config
+    cd $WORKDIR/$REPO_FLODER
     echo -e "\nCONFIG_DEVEL=y\nCONFIG_BUILD_LOG=y\nCONFIG_BUILD_LOG_DIR=\"./logs\"" >> .config
     make defconfig
 }
 
-make_download () {
-    cd $GITHUB_WORKSPACE/$REPO_FLODER
-    echo -e "$green>>>> download packages ... $white"
-    dl_starttime=`date +'%Y-%m-%d %H:%M:%S'`
+MAKE_DOWNLOAD () {
+    cd $WORKDIR/$REPO_FLODER
+    echo -e "$GREEN>>>> download packages ... $WHITE"
+    DL_STARTTIME=`date +'%Y-%m-%d %H:%M:%S'`
     make download -j8
     find dl -size -1024c -exec ls -l {} \;
     find dl -size -1024c -exec rm -f {} \;
-    dl_endtime=`date +'%Y-%m-%d %H:%M:%S'`
+    DL_ENDTIME=`date +'%Y-%m-%d %H:%M:%S'`
 }
 
-clean_up () {
-    cd $GITHUB_WORKSPACE/$REPO_FLODER
-    mv bin/* $GITHUB_WORKSPACE/bin/
-    make clean
-
-    if [ -d files ]; then
-        rm -rf files
-    fi
-
-    if [ -d logs ]; then
-        rm -if logs
-    fi
-
-    if [ -d tmp ]; then
-        rm -rf tmp
-    fi
-
-    if [ -d package/diy ]; then
-        rm -rf diy
-    fi
-
-    if [ -d dl/go-mod-cache ]; then
-        rm -rf dl/go-mod-cache
-    fi
-
-    git reset --hard
-
-    cd $GITHUB_WORKSPACE/$CONFIG_FLODER
-    git reset --hard
-}
-
-make_compile () {
-    cd $GITHUB_WORKSPACE/$REPO_FLODER
-    echo -e "$green>>>> $(nproc) thread compile ... $white"
-    starttime=`date +'%Y-%m-%d %H:%M:%S'`
+MAKE_COMPILE () {
+    cd $WORKDIR/$REPO_FLODER
+    echo -e "$GREEN>>>> $(nproc) thread compile ... $WHITE"
+    STARTTIME=`date +'%Y-%m-%d %H:%M:%S'`
     make -j$(nproc)
     if [ $? -eq 0 ]; then
-        endtime=`date +'%Y-%m-%d %H:%M:%S'`
-        start_seconds=$(date --date="$starttime" +%s);
-        end_seconds=$(date --date="$endtime" +%s);
-        dl_start_seconds=$(date --date="$dl_starttime" +%s);
-        dl_end_seconds=$(date --date="$dl_endtime" +%s);
-        # clean_up
-        echo -e "$green>>>> compile success ... $white"
-        echo -e "$green>>>> download with $((dl_end_seconds-dl_start_seconds))s $white"
-        echo -e "$green>>>> compile with $((end_seconds-start_seconds))s $white"
+        ENDTIME=`date +'%Y-%m-%d %H:%M:%S'`
+        START_SECONDS=$(date --date="$STARTTIME" +%s);
+        END_SECONDS=$(date --date="$ENDTIME" +%s);
+        DL_START_SECONDS=$(date --date="$DL_STARTTIME" +%s);
+        DL_END_SECONDS=$(date --date="$DL_ENDTIME" +%s);
+        echo -e "$GREEN>>>> compile success ... $WHITE"
+        echo -e "$GREEN>>>> download with $((DL_END_SECONDS-DL_START_SECONDS))s $WHITE"
+        echo -e "$GREEN>>>> compile with $((END_SECONDS-START_SECONDS))s $WHITE"
     else
-        echo -e "$red>>>> $(nproc) thread compile failed ... \n$yellow>>>> logs will be updated later ... $white"
+        echo -e "$RED>>>> $(nproc) thread compile failed ... \n$YELLOW>>>> logs will be updated later ... $WHITE"
         exit 1
     fi
 }
 
-update_config_repo
-update_source_code
-custom_configuration
-copy_config
-rm_dl
-make_download
-make_compile
+UPDATE_CONFIG_REPO
+UPDATE_SOURCE_CODE
+CUSTOM_CONFIGURATION
+COPY_CONFIG
+RM_DL
+MAKE_DOWNLOAD
+MAKE_COMPILE
 
--- script/download-openwrt.sh
+++ script/download-openwrt.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-green="\033[32m"
-white="\033[0m"
-yellow="\033[33m"
-red="\033[31m"
+GREEN="\033[32m"
+WHITE="\033[0m"
+YELLOW="\033[33m"
+RED="\033[31m"
 
-rclone lsf $RCLONE_NAME:openwrt/repack > list.txt
-grep "$TARGET_NAME" list.txt > /dev/null
+rclone lsf $RCLONE_NAME:openwrt/repack >list.txt
+grep "$TARGET_NAME" list.txt >/dev/null
 if [ $? -eq 0 ]; then
     rclone copy $RCLONE_NAME:openwrt/repack/$TARGET_NAME-openwrt.tar . --transfers=8 -v
-    tar -xvf $TARGET_NAME-openwrt.tar
+    tar -xvf $TARGET_NAME-openwrt.tar 2>&1 >/dev/null
     rm *-openwrt.tar
-    sudo chown -R $USER:$GROUPS openwrt
+    sudo chown -R $USER:$GROUPS $REPO_FLODER
 else
-    echo -e "$yellow>>>> skip download ... $white"
+    echo -e "$YELLOW>>>> skip download ... $WHITE"
     exit 0
 fi
 
